a53e468c9c18b53305f7ed03c4f2e9980b8da573
Brandon Crisp <brandonc577@gmail.com>
2024-05-09 01:10:55 -0400

    settings

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,37 @@
-import SettingsButton from './components/SettingsButton'
 import PlayerSelect from './components/PlayerSelect'
 import ExpensesCard from './components/ExpensesCard'
 import BalanceCard from './components/BalanceCard'
 import PassiveIncomeCard from './components/PassiveIncomeCard'
 import ChildrenCard from './components/ChildrenCard'
 import PlayerAssetTabs from './components/PlayerAssetTabs'
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from './components/ui/dropdown-menu'
+import { Button } from './components/ui/button'
+import { RotateCcw, Settings } from 'lucide-react'
+import { useDispatch } from 'react-redux'
+import { reset } from './app/dataSlice'
 
 const App = () => {
+
+  const dispatch = useDispatch()
+
   return (
     <div className="flex min-h-screen w-full flex-col bg-muted/40">
       <div className="flex flex-col sm:gap-4 sm:py-4">
         <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
           <PlayerSelect />
-          <SettingsButton className='ml-auto' />
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="ghost" size="icon" className={'w-9 h-9 ml-auto'}>
+                <Settings className='w-5 h-5' />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align='end'>
+              <DropdownMenuItem onSelect={() => dispatch(reset())}>
+                <RotateCcw className='w-4 h-4 mr-2' />
+                New Game
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
         </header>
         <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8 lg:grid-cols-3 xl:grid-cols-3">
           <div className="grid auto-rows-max items-start gap-4 md:gap-8 lg:col-span-2">
